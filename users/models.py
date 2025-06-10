adb0f0ae69267f47d1ac8bb0951cca4aa1c28c51
romz987pc <eesistance@gmail.com>
2025-06-10 12:02:37 +0300

    feat: create and fill out User model;

--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+# my imports 
+from django.contrib.auth.models import AbstractUser
+
+
+NULLABLE = {'blank': True, 'null': True}
+
+class User(AbstractUser):
+    username = None 
+    email = models.EmailField(unique=True, verbose_name='email')
+    phone = models.CharField(max_length=35, unique=True ,verbose_name='phone number', **NULLABLE)
+    telegram = models.CharField(max_length=150, unique=True, verbose_name='telegram username', **NULLABLE)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ''
+
+    def __str__(self):
+        return f'{self.email}'
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+        ordering = ['id']
