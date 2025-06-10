df1645108647db9a94d0a4fd4efa11304a73d4ba
romz987pc <eesistance@gmail.com>
2025-06-10 14:14:52 +0300

    feat: register user model in admin panel method 2

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from users.models import User
 
 # model registration in admin panel
-admin.site.register(User)
+
+# method one: common
+# admin.site.register(User)
+
+# method two: detailed 
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'pk')
+    list_filter = ('last_name', )
