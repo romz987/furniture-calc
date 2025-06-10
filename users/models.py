523f89f74af34561d49f2642c77885b0f6e23e99
romz987pc <eesistance@gmail.com>
2025-06-10 12:29:24 +0300

    fix: fix up required_fields str -> list;

--- users/models.py
+++ users/models.py
@@ -13,7 +13,7 @@ class User(AbstractUser):
     telegram = models.CharField(max_length=150, unique=True, verbose_name='telegram username', **NULLABLE)
     
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ''
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return f'{self.email}'
