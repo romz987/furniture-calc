a1caaf81d17ea60fd2101226928dcc53015d4875
romz987pc <eesistance@gmail.com>
2025-06-12 13:29:07 +0300

    config: 1.fill in static dir settings;  2.fill in media dir settings;

--- config/settings.py
+++ config/settings.py
@@ -139,6 +139,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# my changes 
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
+# uploaded media 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = (
+    BASE_DIR / 'media',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
