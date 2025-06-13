a78cb6db870e9d627534ca2606eee8d5820ff998
romz987pc <eesistance@gmail.com>
2025-06-13 23:49:47 +0300

    config: fix up media root dir;

--- furniture/models.py
+++ furniture/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Furniture(models.Model):
     name = models.CharField(max_length=100, verbose_name='furniture type')
     description = models.CharField(max_length=350, verbose_name='type description')
+    image = models.ImageField(upload_to='furniture/', verbose_name='image')
 
     def __str__(self):
         return f'{self.name}'
