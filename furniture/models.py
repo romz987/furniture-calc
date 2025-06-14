6c886a2e0d3cde6ddc431e2a1d9c3222bc8fe97e
romz987pc <eesistance@gmail.com>
2025-06-14 10:58:02 +0300

    feat: add image field to furniture model;

--- furniture/models.py
+++ furniture/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 
+NULLABLE = {'blank': True, 'null': True}
+
 class Furniture(models.Model):
     name = models.CharField(max_length=100, verbose_name='furniture type')
     description = models.CharField(max_length=350, verbose_name='type description')
-    image = models.ImageField(upload_to='furniture/', verbose_name='image')
+    image = models.ImageField(upload_to='furniture/', verbose_name='image', **NULLABLE)
 
     def __str__(self):
         return f'{self.name}'
