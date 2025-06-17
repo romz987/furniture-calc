9927b489ff9db9827bc0b7757095627b50d749b5
romz987pc <eesistance@gmail.com>
2025-06-17 13:41:53 +0300

    fix: remove unexisting view import;

--- furniture/urls.py
+++ furniture/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from furniture.apps import FurnitureConfig
-from furniture.views import index, furniture_list_view
+from furniture.views import index
 
 app_name = FurnitureConfig.name
 
