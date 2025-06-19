f635b46956a6f86b40757e45d2c3c775bf156e8e
romz987pc <eesistance@gmail.com>
2025-06-19 19:40:22 +0300

    feat: add combination_not_found url;

--- wardrobe/urls.py
+++ wardrobe/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path 
 from wardrobe.apps import WardrobeConfig
-from wardrobe.views import wardrobe
+from wardrobe.views import wardrobe, WardrobeView, combination_not_found
 
 app_name = WardrobeConfig.name
 
 
 urlpatterns = [
-    path('wardrobe/', wardrobe, name='calculator'),
+    path('wardrobe/', WardrobeView.as_view(), name='calculator'),
+    path('combination_not_found/', combination_not_found, name='combination_not_found'),
 ]
