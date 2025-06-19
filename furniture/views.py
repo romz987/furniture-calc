dd0d0471e0b920c725acd609de36dcc57d13977c
romz987pc <eesistance@gmail.com>
2025-06-19 20:08:26 +0300

    feat: 1.add CBV index view;  2.remove FBV index view;

--- furniture/views.py
+++ furniture/views.py
@@ -1,9 +1,25 @@
+from django.urls import reverse
+from django.views import View
 from django.shortcuts import render
 from furniture.models import Furniture 
 
 
-def index(request):
-    context = {
-        'objects_list': Furniture.objects.all()
+class FurnitureListView(View):
+    template = 'furniture/index.html'
+
+    calc_urls = {
+        'шкаф': 'wardrobe:calculator',
+        'кухня': 'wardrobe:calculator',
+        'комод': 'wardrobe:calculator'
     }
-    return render(request, 'furniture/index.html', context=context)
+
+    def get(self, request):
+        objects = Furniture.objects.all()
+        # Добавлеяем url в каждый объект из базы
+        for obj in objects:
+            url_name = self.calc_urls[str(obj.name).lower()]
+            obj.calculator_url = (
+                reverse(url_name)
+            ) 
+        context = {'objects_list': objects}
+        return render(request, self.template, context)
