66390bc7f385b64ed6766963debf96b6f7c1ac08
romz987pc <eesistance@gmail.com>
2025-06-20 17:38:34 +0300

    feat: add save_order url;

--- wardrobe/urls.py
+++ wardrobe/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path 
 from wardrobe.apps import WardrobeConfig
-from wardrobe.views import wardrobe, WardrobeView, combination_not_found
+from wardrobe.views import WardrobeView, WardrobeSaveOrderView, combination_not_found
 
 app_name = WardrobeConfig.name
 
@@ -8,4 +8,5 @@ app_name = WardrobeConfig.name
 urlpatterns = [
     path('wardrobe/', WardrobeView.as_view(), name='calculator'),
     path('combination_not_found/', combination_not_found, name='combination_not_found'),
+    path('save_order/', WardrobeSaveOrderView.as_view(), name='save_order'),
 ]
