d618e38668b5e1ff9fe046d6e7f18efa72c4029d
romz987pc <eesistance@gmail.com>
2025-06-23 12:05:48 +0300

    feat: 1. add registration success path;  2. add password-recovery path;

--- users/urls.py
+++ users/urls.py
@@ -8,4 +8,6 @@ app_name = UsersConfig.name
 urlpatterns = [
     path('register/', user_register_view, name='user_register'),
     path('login/', user_login_view, name='user_login'),
+    path('registration-success/', successful_register, name='user_registered'),
+    path('password-recovery', restore_password, name='password_recovery')
 ]
