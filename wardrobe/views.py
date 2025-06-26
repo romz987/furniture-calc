03c789974a09931957b20a2e1e5c35bce4eca18c
romz987pc <eesistance@gmail.com>
2025-06-26 16:53:29 +0300

    fix: fix up all CBV's;

--- wardrobe/views.py
+++ wardrobe/views.py
@@ -1,22 +1,45 @@
-from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-from .forms import WardrobeForm, SaveOrderForm
 from django.views import View
-from reference.models import BoxSummary, DoorSummary, DoorHandle
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from reference.models import (
+    MaterialType,
+    MaterialThickness,
+    MaterialColor,
+    DoorType,
+    BoxSummary, 
+    DoorSummary, 
+    DoorHandle,
+)
 from .services import CalculateWardrobe
+from .forms import WardrobeForm, SaveOrderForm
+from .models import Orders
 
 
-class WardrobeView(View):
+class WardrobeView(LoginRequiredMixin, View):
     template = 'wardrobe.html'
     template_result = 'wardrobe_result.html'
-
+    model_mtypes = MaterialType
+    model_thicknesses = MaterialThickness
+    model_colors = MaterialColor
+    model_dtypes = DoorType
+    model_box_summary = BoxSummary
+    model_door_summary = DoorSummary
+    model_door_handle = DoorHandle
+    form_class = WardrobeForm
 
     def get(self, request):
-        form = WardrobeForm()
-        return render(request, self.template, {'form': form})
+        form = self.form_class(**self.get_form_kwargs())
+        context = {
+            'form': form,
+        }
+        return render(request, self.template, context=context)
 
     def post(self, request):
-        form = WardrobeForm(request.POST)
+        form = self.form_class(request.POST, **self.get_form_kwargs())
         if form.is_valid():
             # Упаковываем данные из запроса в два словаря
             info, size = self.extract_data(form)
@@ -29,11 +52,16 @@ class WardrobeView(View):
             if not info["door_price_per_sqm"]:
                 messages.error(request, "Комбинация для двери не найдена")
                 return redirect('wardrobe:combination_not_found')
-            # Получаем стоимость выбранной ручки 
-            info["handle_price"] = self.get_handle_price(info)
+            # Получаем данные выбранной ручки 
+            info.update(self.get_handle_data(info))
             # Считаем
             calculator = CalculateWardrobe()
             size, info = calculator.calculate_price(size, info)
+            # Нормализация
+            info = self.normalize_info(info)
+            # Сохраняем результат в сессии
+            request.session['wardrobe_size'] = size 
+            request.session['wardrobe_info'] = info
             # На страницу результатов
             return render(
                 request, 
@@ -42,15 +70,55 @@ class WardrobeView(View):
             )
         return render(request, self.template, {'form': form})
 
+    def get_form_kwargs(self):
+        kwargs = {}
+        # uq_box_materials
+        kwargs["materials"] = self.get_form_data(
+            self.model_mtypes, self.model_box_summary, "material_type")
+        # uq_box_thicknesses
+        kwargs["box_thicknesses"] = self.get_form_data(
+            self.model_thicknesses, self.model_box_summary, "material_thickness")
+        # uq_box_colors
+        kwargs["box_colors"] = self.get_form_data(
+            self.model_colors, self.model_box_summary, "material_color")
+        # uq_door_thicknesses
+        kwargs["door_thicknesses"] = self.get_form_data(
+            self.model_thicknesses, self.model_door_summary, "material_thickness")
+        # uq_door_colors
+        kwargs["door_colors"] = self.get_form_data(
+            self.model_colors, self.model_door_summary, "material_color")
+        # uq_door_types
+        kwargs["door_types"] = self.get_form_data(
+            self.model_dtypes, self.model_door_summary, "door_type")
+        return kwargs
+
+    def get_form_data(self, model_one, model_two, field_name):
+        """
+        Get distinct used data 
+        :param model_one: main model
+        :param_model_two: summary model
+        :param field_name: field name
+        """
+        result = model_one.objects.filter(
+            id__in=model_two.objects.values_list(
+                field_name, flat=True
+            )
+        ).distinct()
+        return result
+
     def extract_data(self, form):
         info = {
-            "material_type": form.cleaned_data["material"],
-            "material_thickness": form.cleaned_data["thickness"],
-            "material_color": form.cleaned_data["color"],
-            "door_type": form.cleaned_data["door_type"],
-            # id толщины двери
-            "door_thickness": 1,
-            "handle_type": form.cleaned_data["handle_type"]
+            # короб
+            "material_type": form.cleaned_data["materials"],
+            "box_material_thickness": form.cleaned_data["box_thicknesses"],
+            "box_material_color": form.cleaned_data["box_colors"],
+            # двери
+            "door_type": form.cleaned_data["door_types"],
+            "door_material_thickness": form.cleaned_data["door_thicknesses"],            
+            "door_material_color": form.cleaned_data["door_colors"],  
+            # фурнитура
+            "handle_name": form.cleaned_data["handle_name"],
+            "handle_ammount": 2
         }
         size = {
             "height": int(form.cleaned_data["height"]),
@@ -60,36 +128,216 @@ class WardrobeView(View):
         return info, size
 
     def get_box_material_price(self, info):
-        box = BoxSummary.objects.filter(
+        box = self.model_box_summary.objects.filter(
             material_type=info["material_type"],
-            material_thickness=info["material_thickness"],
-            material_color=info["material_color"]
+            material_thickness=info["box_material_thickness"],
+            material_color=info["box_material_color"]
         ).first()
         return box.price_per_sqm if box else None
 
     def get_door_material_price(self, info):
-        door = DoorSummary.objects.filter(
+        door = self.model_door_summary.objects.filter(
             material_type=info["material_type"],
-            material_thickness=info["door_thickness"],
-            material_color=info["material_color"],
+            material_thickness=info["door_material_thickness"],
+            material_color=info["door_material_color"],
             door_type=info["door_type"]
         ).first()
         return door.price_per_sqm if door else None
 
-    def get_handle_price(self, info):
-        handle = DoorHandle.objects.filter(
-            name=info["handle_type"]
+    def get_handle_data(self, info):
+        handle = self.model_door_handle.objects.filter(
+            name=info["handle_name"]
         ).first()
-        return handle.price_per_one 
+        return {
+            "handle_price_per_one": handle.price_per_one, 
+            "handle_length": handle.length, 
+            "handle_color": handle.color, 
+            "handle_material": handle.material
+        } 
+
+    def normalize_info(self, info):
+        # короб
+        info["material_type"] = info["material_type"].name
+        info["box_material_thickness"] = info["box_material_thickness"].thickness
+        info["box_material_color"] = info["box_material_color"].name
+        # двери
+        info["door_type"] = info["door_type"].name
+        info["door_material_thickness"] = info["door_material_thickness"].thickness
+        info["door_material_color"] = info["door_material_color"].name 
+        # фурнитура
+        info["handle_name"] = info["handle_name"].name
+        # остальные
+        info["box_price_per_sqm"] = int(info["box_price_per_sqm"])
+        info["door_price_per_sqm"] = int(info["door_price_per_sqm"])
+        info["handle_price_per_one"] = int(info["handle_price_per_one"])
+        info["handle_price"] = int(info["handle_price"])
+        info["box_price"] = int(info["box_price"])
+        info["door_price"] = int(info["door_price"])
+        return info
 
 
-class WardrobeSaveOrderView(View):
+class WardrobeSaveOrderView(LoginRequiredMixin, View):
     template = 'wardrobe_save_order.html'
+    model_orders = Orders
+    form_class = SaveOrderForm
 
     def get(self, request):
-        form = SaveOrderForm()
+        form = self.form_class()
         return render(request, self.template, {'form': form})
 
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            data = self.get_data_from_session(request)
+            if isinstance(data, HttpResponseRedirect):
+                return data
+            size, info = data
+            self.save_order(request, form, size, info)
+        return redirect('wardrobe:save_order_success')
+    
+    def get_data_from_session(self, request):
+        size = request.session.get('wardrobe_size')
+        info = request.session.get('wardrobe_info')
+        if not size or not info:
+            messages.error(request, "Данные расчёта не найдены.")
+            return redirect('wardrobe:calculator')
+        return size, info
 
-def combination_not_found(request):
+    def save_order(self, request, form, size, info):
+        order = self.model_orders.objects.create(
+            # заказчик
+            customer_name=form.cleaned_data['customer_name'],
+            customer_surname=form.cleaned_data['customer_surname'],
+            phone=form.cleaned_data['phone'],
+            email=form.cleaned_data['email'],
+            # размеры
+            height=size['height'],
+            width=size['width'],
+            depth=size['depth'],
+            # короб
+            material_type=info['material_type'],
+            box_material_thickness=info['box_material_thickness'],
+            box_material_color=info['box_material_color'],
+            box_square=size['box_square'],
+            box_price_per_sqm=info['box_price_per_sqm'],
+            box_price=info['box_price'],
+            # двери
+            door_type=info['door_type'],
+            door_material_thickness=info['door_material_thickness'],
+            door_material_color=info['door_material_color'],
+            door_square=size['door_square'],
+            door_price_per_sqm=info['door_price_per_sqm'],
+            door_price=info['door_price'],
+            # фурнитура
+            handle_name=info["handle_name"],
+            handle_material=info["handle_material"],
+            handle_color=info["handle_color"],
+            handle_length=info["handle_length"],
+            handle_price_per_one=info["handle_price_per_one"],
+            handle_ammount=info["handle_ammount"],
+            handle_price=info["handle_price"],
+            # итоговая цена
+            total_price=info['total_price'],
+            # владелец
+            owner=request.user,
+            # дата
+            order_date=timezone.now()
+        )
+        messages.success(request, 'заказ успешно сохранен')
+
+
+class WardrobeOrderDetailView(LoginRequiredMixin, View):
+    template = 'wardrobe_order_details.html'
+    model_orders = Orders
+
+    def get(self, request, pk):
+        order_record = get_object_or_404(self.model_orders, pk=pk)
+        customer_data = self.prepare_customer_data(order_record)
+        order_size = self.prepare_order_size(order_record)
+        order_info = self.prepare_order_info(order_record)
+        context = {
+            'customer_data': customer_data,
+            'order_size': order_size,
+            'order_info': order_info,
+        }
+        return render(request, self.template, context=context)
+
+    def prepare_customer_data(self, order_record):
+        return {
+            'order_id': order_record.id,
+            'customer_name': order_record.customer_name,
+            'customer_surname': order_record.customer_surname,
+            'phone': order_record.phone,
+            'email': order_record.email,
+            'order_date': order_record.order_date,
+        }
+
+    def prepare_order_size(self, order_record):
+        return {
+            'height': order_record.height,
+            'width': order_record.width,
+            'depth': order_record.depth,
+            'box_square': order_record.box_square,
+            'door_square': order_record.door_square,
+        }
+
+    def prepare_order_info(self, order_record):
+        return {
+            'material_type': order_record.material_type,
+            'box_material_thickness': order_record.box_material_thickness,
+            'box_material_color': order_record.box_material_color,
+            'box_price_per_sqm': order_record.box_price_per_sqm,
+            'box_square': order_record.box_square,
+            'box_price': order_record.box_price,
+            # двери
+            'door_type': order_record.door_type,
+            'door_material_thickness': order_record.door_material_thickness,
+            'door_material_color': order_record.door_material_color,
+            'door_price_per_sqm': order_record.door_price_per_sqm,
+            'door_square': order_record.door_square,
+            'door_price': order_record.door_price,
+            # фурнитура
+            'handle_name': order_record.handle_name,
+            'handle_material': order_record.handle_material,
+            'handle_color': order_record.handle_color,
+            'handle_length': order_record.handle_length,            
+            'handle_price_per_one': order_record.handle_price_per_one,
+            'handle_ammount': order_record.handle_ammount,
+            'handle_price': order_record.handle_price,
+            # итоговая цена
+            'total_price': order_record.total_price,
+        }
+
+
+    def normalize_info(self, info):
+        info["material_type"] = info["material_type"].name
+        info["material_thickness"] = info["material_thickness"].thickness
+        info["material_color"] = info["material_color"].name
+        info["door_type"] = info["door_type"].name
+        info["handle_type"] = info["handle_type"].name
+        info["box_price_per_sqm"] = int(info["box_price_per_sqm"])
+        info["door_price_per_sqm"] = int(info["door_price_per_sqm"])
+        info["handle_price"] = int(info["handle_price"])
+        info["box_price"] = int(info["box_price"])
+        info["door_price"] = int(info["door_price"])
+        return info
+
+
+@login_required
+def combination_not_found_view(request):
     return render(request, 'combination_not_found.html', {'title': 'Ошибка'})
+
+
+@login_required
+def save_order_success_view(request):
+    return render(request, 'wardrobe_save_order_success.html')
+
+
+@login_required
+def orders_list_view(request):
+    objects_list = Orders.objects.all()
+    context = {
+        'title': 'Заказы шкафов',
+        'objects_list': objects_list
+    }
+    return render(request, 'wardrobe_orders_list.html', context=context)
