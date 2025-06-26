6479c30a5ca3353e645a5d0b2b841d93b8237ced
romz987pc <eesistance@gmail.com>
2025-06-26 16:51:34 +0300

    fix: fix up WardrobeForm;

--- wardrobe/forms.py
+++ wardrobe/forms.py
@@ -9,15 +9,36 @@ from reference.models import (
 
 
 class WardrobeForm(forms.Form):
-    height = forms.CharField(label='Высота мм', widget=forms.TextInput(attrs={"class": "form-control"}))
-    width = forms.CharField(label='Ширина мм', widget=forms.TextInput(attrs={"class": "form-control"}))
-    depth = forms.CharField(label='Глубина мм', widget=forms.TextInput(attrs={"class": "form-control"}))
+    # Рамеры
+    height = forms.CharField(label='высота мм', widget=forms.TextInput(attrs={"class": "form-control"}))
+    width = forms.CharField(label='ширина мм', widget=forms.TextInput(attrs={"class": "form-control"}))
+    depth = forms.CharField(label='глубина мм', widget=forms.TextInput(attrs={"class": "form-control"}))
+    # Короб
+    materials = forms.ModelChoiceField(queryset=MaterialType.objects.none(), label="Материал", widget=forms.Select(attrs={"class": "form-control"}))
+    box_thicknesses = forms.ModelChoiceField(queryset=MaterialThickness.objects.none(), label="Толщина материала", widget=forms.Select(attrs={"class": "form-control"}))
+    box_colors = forms.ModelChoiceField(queryset=MaterialColor.objects.none(), label="Цвет материала", widget=forms.Select(attrs={"class": "form-control"}))
+    # Двери
+    door_types = forms.ModelChoiceField(queryset=DoorType.objects.all(), label="Тип дверей", widget=forms.Select(attrs={"class": "form-control"}))
+    door_thicknesses = forms.ModelChoiceField(queryset=MaterialThickness.objects.none(), label="Толщина дверей", widget=forms.Select(attrs={"class": "form-control"}))
+    door_colors = forms.ModelChoiceField(queryset=MaterialColor.objects.none(), label="Цвет дверей", widget=forms.Select(attrs={"class": "form-control"}))
+    # Фурнтирута
+    handle_name = forms.ModelChoiceField(queryset=DoorHandle.objects.all(), label="Ручки", to_field_name='name', widget=forms.Select(attrs={"class": "form-control"}))
 
-    material = forms.ModelChoiceField(queryset=MaterialType.objects.all(), label="Материал", widget=forms.Select(attrs={"class": "form-control"}))
-    thickness = forms.ModelChoiceField(queryset=MaterialThickness.objects.all(), label="Толщина материала", widget=forms.Select(attrs={"class": "form-control"}))
-    color = forms.ModelChoiceField(queryset=MaterialColor.objects.all(), label="Цвет материала", widget=forms.Select(attrs={"class": "form-control"}))
-    door_type = forms.ModelChoiceField(queryset=DoorType.objects.all(), label="Тип дверей", widget=forms.Select(attrs={"class": "form-control"}))
-    handle_type = forms.ModelChoiceField(queryset=DoorHandle.objects.all(), label="Ручки", to_field_name='name', widget=forms.Select(attrs={"class": "form-control"}))
+    def __init__(self, *args, **kwargs):
+        box_materials = kwargs.pop('materials')
+        box_thicknesses = kwargs.pop('box_thicknesses')
+        box_colors = kwargs.pop('box_colors')
+        door_thicknesses = kwargs.pop('door_thicknesses')
+        door_colors = kwargs.pop('door_colors')
+        door_types = kwargs.pop('door_types')
+        #
+        super().__init__(*args, **kwargs)
+        self.fields['materials'].queryset = box_materials
+        self.fields['box_thicknesses'].queryset = box_thicknesses
+        self.fields['box_colors'].queryset = box_colors
+        self.fields['door_thicknesses'].queryset = door_thicknesses
+        self.fields['door_colors'].queryset = door_colors
+        self.fields['door_types'].queryset = door_types
 
 
 class SaveOrderForm(forms.Form):
