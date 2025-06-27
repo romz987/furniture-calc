b8a9f459700a5a4f392d67c4c79d10858df3aaa2
romz987pc <eesistance@gmail.com>
2025-06-27 12:50:29 +0300

    feat: add url wardrobe_order_update;

--- wardrobe/urls.py
+++ wardrobe/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path 
 from wardrobe.apps import WardrobeConfig
-from wardrobe.views import WardrobeView, WardrobeSaveOrderView, WardrobeOrderDetailView, combination_not_found_view, save_order_success_view, orders_list_view, order_delete_view
+from wardrobe.views import WardrobeView, WardrobeSaveOrderView, WardrobeOrderDetailView, combination_not_found_view, save_order_success_view, orders_list_view, order_delete_view, OrderUpdateView
 
 app_name = WardrobeConfig.name
 
@@ -13,4 +13,5 @@ urlpatterns = [
     path('orders/', orders_list_view, name='show_wardrobe_orders'),
     path('order_detail/<int:pk>/', WardrobeOrderDetailView.as_view(), name='show_wardrobe_order_detail'), 
     path('order_delete/<int:pk>/', order_delete_view, name='order_delete'),
+    path('order_update/<int:pk>/', OrderUpdateView.as_view(), name='wardrobe_order_update')
 ]
