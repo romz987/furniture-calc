506eb8300b6e0f876086c20ea51a67fa635c8e18
romz987pc <eesistance@gmail.com>
2025-07-02 14:31:52 +0300

    feat: 1. UserProfileView;  2. add @login_required;  3. fix up imports order;

--- users/views.py
+++ users/views.py
@@ -1,8 +1,13 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import render, reverse
-from django.http import HttpResponseRedirect, HttpResponse
-from users.forms import UserRegisterForm, UserLoginForm
+from django.urls import reverse_lazy
 from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import DetailView, UpdateView
+from users.forms import UserRegisterForm, UserLoginForm, UserUpdateForm
+from users.models import User
 
 
 def user_register_view(request):
@@ -43,11 +48,22 @@ def successful_register_view(request):
     return render(request, 'users/registration_success.html')
 
 
+@login_required
 def restore_password_view(request):
     return render(request, 'users/password_recovery.html')
 
 
+@login_required
 def user_logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse('users:user_login'))
 
+
+class UserProfileView(LoginRequiredMixin, UpdateView):
+    model = User 
+    form_class = UserUpdateForm 
+    template_name = 'users/user_profile.html'
+    success_url = reverse_lazy('furniture:index')
+
+    def get_object(self, queryset=None):
+        return self.request.user
