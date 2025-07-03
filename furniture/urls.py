5b8fe5b4b64a2d8454bbabc321b92ba1b83eb527
romz987pc <eesistance@gmail.com>
2025-07-03 13:57:45 +0300

    feat: add manuals and plugs urls;

--- furniture/urls.py
+++ furniture/urls.py
@@ -1,10 +1,30 @@
 from django.urls import path
 from furniture.apps import FurnitureConfig
-from furniture.views import IndexView
+from furniture.views import (
+    IndexView,
+    wardrobe_man_view,
+    dresser_man_view,
+    kitchen_man_view,
+    # заглушки
+    dresser_calc_plug_view,
+    kitchen_calc_plug_view,
+    dresser_orders_plug_view,
+    kitchen_orders_plug_view,
+)
 
 app_name = FurnitureConfig.name
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    # мануалы
+    path('wardrobe-manual/', wardrobe_man_view, name='wardrobe_manual'),
+    path('dresser-manual/', dresser_man_view, name='dresser_manual'),
+    path('kitchen-manual/', kitchen_man_view, name='kitchen_manual'),
+    # заглушки калькуляторы
+    path('dresser-calc-plug/', dresser_calc_plug_view, name='dresser_calc_plug'),
+    path('kitchen-calc-plug/', kitchen_calc_plug_view, name='kitchen_calc_plug'),
+    # заглушки заказы
+    path('dresser-orders-plug/', dresser_orders_plug_view, name='dresser_orders_plug'),
+    path('kitchen-orders-plug/', kitchen_orders_plug_view, name='kitchen_orders_plug'),
 ]
