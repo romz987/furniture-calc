defbc8314c0470bb05b8278a5a39e97ee1b10ded
romz987pc <eesistance@gmail.com>
2025-07-09 12:51:43 +0300

    feat: 1. add management urls;  2. fix up views names accoring changes in views.py;

--- wardrobe/urls.py
+++ wardrobe/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path 
 from wardrobe.apps import WardrobeConfig
-from wardrobe.views import WardrobeView, WardrobeSaveOrderView, WardrobeOrderDetailView, combination_not_found_view, save_order_success_view, orders_list_view, order_delete_view, OrderUpdateView, deactivated_list_view
+from wardrobe.views import (
+    WardrobeView, 
+    WardrobeSaveOrderView,
+    WardrobeOrderDetailView, 
+    combination_not_found_view, 
+    save_order_success_view, 
+    wardrobe_orders_list_view, 
+    wardrobe_order_delete_view, 
+    WardrobeOrderUpdateView, 
+    wardrobe_deactivated_list_view,
+    toggle_order_active_view,
+    WardrobeDeactivatedDetailView,
+)
 
 app_name = WardrobeConfig.name
 
@@ -10,10 +22,12 @@ urlpatterns = [
     path('combination_not_found/', combination_not_found_view, name='combination_not_found'),
     path('save_order/', WardrobeSaveOrderView.as_view(), name='save_order'),
     path('save_order_success/', save_order_success_view, name='save_order_success'),
-    path('orders/', orders_list_view, name='show_wardrobe_orders'),
+    path('orders/', wardrobe_orders_list_view, name='show_wardrobe_orders'),
     path('order_detail/<int:pk>/', WardrobeOrderDetailView.as_view(), name='show_wardrobe_order_detail'), 
-    path('order_delete/<int:pk>/', order_delete_view, name='order_delete'),
-    path('order_update/<int:pk>/', OrderUpdateView.as_view(), name='wardrobe_order_update'),
-    # admin
-    path('management/wardrobe-deactivated/', deactivated_list_view, name='wardrobe_deactivated')
+    path('order_delete/<int:pk>/', wardrobe_order_delete_view, name='order_delete'),
+    path('order_update/<int:pk>/', WardrobeOrderUpdateView.as_view(), name='wardrobe_order_update'),
+    # management
+    path('management/wardrobe-deactivated/', wardrobe_deactivated_list_view, name='wardrobe_deactivated'),
+    path('management/togggle-order-activity/<int:pk>/', toggle_order_active_view, name='toggle_order_activity'),
+    path('management/wardrobe-deactivated-details/<int:pk>/', WardrobeDeactivatedDetailView.as_view(), name='wardrobe_deactivated_details'),
 ]
