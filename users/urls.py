021b7f4b04ec65f5f822141fb605e5e1cc8bde89
romz987pc <eesistance@gmail.com>
2025-07-09 13:14:48 +0300

    fix:  1. add apparent imports;  2. add managements urls;

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path 
 from users.apps import UsersConfig  
-from users.views import *
+from users.views import (
+    user_register_view,
+    user_login_view,
+    successful_register_view,
+    restore_password_view,
+    user_logout_view,
+    UserProfileView,
+    manage_users_view,
+    toggle_user_active_view,
+)
 
 app_name = UsersConfig.name 
 
@@ -14,5 +23,5 @@ urlpatterns = [
     path('profile/', UserProfileView.as_view(), name='user_profile'),
     # admin
     path('management/manage-users/', manage_users_view, name='manage_users'),
-    path('management/togggle-activity/<int:pk>/', toggle_user_active_view, name="toggle_activity")
+    path('management/togggle-user-activity/<int:pk>/', toggle_user_active_view, name="toggle_user_activity")
 ]
