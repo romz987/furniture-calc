059585f3fbd21550fb3bfeb5c79b17fee51a49eb
romz987pc <eesistance@gmail.com>
2025-07-10 11:26:13 +0300

    feat: 1. add ActiveInvites model;  2. add invite_number field to User model;

--- users/models.py
+++ users/models.py
@@ -11,7 +11,8 @@ class User(AbstractUser):
     email = models.EmailField(unique=True, verbose_name='Email')
     phone = models.CharField(max_length=12, unique=True ,verbose_name='Номер телефона', blank=True, null=True)
     telegram = models.CharField(max_length=150, unique=True, verbose_name='Telegram', blank=True, null=True)
-    is_active = models.BooleanField(default=True, verbose_name='active')
+    is_active = models.BooleanField(default=True, verbose_name='Активность')
+    invite_number = models.CharField(max_length=50, verbose_name='Инвайт', blank=True, null=True)
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
@@ -23,3 +24,14 @@ class User(AbstractUser):
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
         ordering = ['id']
+
+
+class ActiveInvites(models.Model):
+    invite_number = models.CharField(max_length=50, verbose_name='Инвайт', blank=True, null=True) 
+
+    def __str__(self):
+        return f'{self.invite_number}'
+
+    class Meta:
+        verbose_name = 'Инвайт'
+        verbose_name_plural = 'Инвайты'
