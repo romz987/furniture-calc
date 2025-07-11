c9a523c4cca5f2cba52ed3f11758cc13fdde3942
romz987pc <eesistance@gmail.com>
2025-07-11 14:27:32 +0300

    feat: 1. add activate-account url;  2. fix up imports order;  3. fix up urls names;

--- users/urls.py
+++ users/urls.py
@@ -1,33 +1,37 @@
-from django.urls import path 
-from users.apps import UsersConfig  
+from django.urls import path
+from users.apps import UsersConfig
 from users.views import (
     user_register_view,
     user_login_view,
     successful_register_view,
     restore_password_view,
     user_logout_view,
-    UserProfileView,
     manage_users_view,
     toggle_user_active_view,
-    GenerateInviteView,
     invite_details_view,
     delete_invite_view,
+    activate_account_view,
+    UserProfileView,
+    GenerateInviteView,
+    UserRegisterView,
 )
 
-app_name = UsersConfig.name 
+app_name = UsersConfig.name
 
 
 urlpatterns = [
-    path('register/', user_register_view, name='user_register'),
-    path('login/', user_login_view, name='user_login'),
-    path('registration-success/', successful_register_view, name='user_registered'),
-    path('password-recovery/', restore_password_view, name='password_recovery'),
-    path('logout/', user_logout_view, name='user_logout'),
-    path('profile/', UserProfileView.as_view(), name='user_profile'),
+    path('register/', UserRegisterView.as_view(), name='user-register'),
+    path('login/', user_login_view, name='user-login'),
+    path('registration-success/', successful_register_view, name='user-registered'),
+    path('password-recovery/', restore_password_view, name='password-recovery'),
+    path('logout/', user_logout_view, name='user-logout'),
+    path('profile/', UserProfileView.as_view(), name='user-profile'),
     # admin
-    path('management/manage-users/', manage_users_view, name='manage_users'),
-    path('management/togggle-user-activity/<int:pk>/', toggle_user_active_view, name="toggle_user_activity"),
-    path('management/generate-invite/', GenerateInviteView.as_view(), name="generate_invite"),
+    path('management/manage-users/', manage_users_view, name='manage-users'),
+    path('management/togggle-user-activity/<int:pk>/', toggle_user_active_view, name="toggle-user-activity"),
+    path('management/generate-invite/', GenerateInviteView.as_view(), name="generate-invite"),
     path('management/invite-details/<int:pk>/', invite_details_view, name="invite-details"),
-    path('management/delete_invite/<int:pk>/', delete_invite_view, name="delete_invite")
+    path('management/delete_invite/<int:pk>/', delete_invite_view, name="delete-invite"),
+    # activate account
+    path('activate/<uidb64>/<token>/', activate_account_view, name='activate-account'),
 ]
