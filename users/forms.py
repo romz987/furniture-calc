cf9a1ef3d899c5330a816e43ad99f4096c478bd5
romz987pc <eesistance@gmail.com>
2025-07-12 12:43:41 +0300

    feat: add MySetPasswordForm for password reset;

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
 from django import forms 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, SetPasswordForm
 from users.models import User 
 
 
@@ -37,3 +37,11 @@ class UserUpdateForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs.update({"class": "form-control"})
+
+
+class MySetPasswordForm(SetPasswordForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control'})
