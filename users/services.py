1800bebfae5c8350debb8ca8a9d823d86fda2a01
romz987pc <eesistance@gmail.com>
2025-07-12 12:44:34 +0300

    fix: fix up subjects and messages;

--- users/services.py
+++ users/services.py
@@ -3,35 +3,18 @@ from django.core.mail import send_mail
 
 
 def send_register_verification_email(email, activation_link):
-    subject = 'Calcoo: Подтверждение регистрации'
+    subject = 'Calcoo: подтверждение регистрации'
     message = (
-        f'Для подтверждения регистрации на сервисе Calcoo пройдите по ссылке:\n'
+        f'Для подтверждения адреса электронной почты пройдите по ссылке:\n'
         f'{activation_link}'
     )
     send_email(email, subject, message)
 
 
 def send_register_success_email(email):
-    subject = 'Calcoo: Успешная регистрация пользователя на сервисе'
+    subject = 'Calcoo: успешная регистрация пользователя сервисе'
     message = (
         'Ваш аккаунт активирован'
-    )
-    send_email(email, subject, message)
-
-
-def send_password_recovery_email(email, recovery_url):
-    subject = 'Calcoo: Запрос на восстановление пароля'
-    message = (
-        f'Для восстановения пароля на сервисе Calcoo пройдите по ссылке:\n'
-        f'{recovery_url}'
-    )
-    send_email(email, subject, message)
-
-
-def send_recovery_success_email(email):
-    subject = 'Calcoo: успешное изменение пароля'
-    message = (
-        'Пароль успешно изменен!'
     )
     send_email(email, subject, message)
 
