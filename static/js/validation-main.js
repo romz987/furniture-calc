6816b5e82e7cbad9eb837bd51b08a76a6bab2847
romz987pc <eesistance@gmail.com>
2025-07-13 00:22:21 +0300

    feat: 1. add validator for login_page;  2. add validator fot registartion page;  3. add validator for password reset pages;

--- static/js/validation-main.js
+++ static/js/validation-main.js
@@ -1,10 +1,148 @@
 document.addEventListener('DOMContentLoaded', function () {
   validateWardrobeForm(); 
   validateSaveOrderForm();
+  validateLoginForm();
+  validateRegisterForm();
+  validateResetPassordForm();
+  validateSetPasswordForm();
 });
 
 
-// Валидатор страницы калькулятора
+/**
+Password Set Form Validator
+*/
+function validateSetPasswordForm() {
+  const form = document.querySelector('.set-password-form');
+  if (!form) return;
+
+  const passwdInputs = form.querySelectorAll('.user-password')
+  const submitBtn = form.querySelector('button[type="submit"]')
+
+  // Отключить кнопку submit 
+  submitBtn.disabled = true;
+  // Проверить валидность полей
+  function checkFormValidity() {
+    let allValid = true;   
+    // Валидация password
+    passwdInputs.forEach(input => {
+      const valid = validatePassword(input.value);
+      if (!valid) allValid = false;
+    });
+
+    // Активируем или деактивируем кнопку
+    submitBtn.disabled = !allValid;
+  };
+  // Добавляем обработчики событий на поля ввода
+  passwdInputs.forEach(input => input.addEventListener('input', checkFormValidity));
+  // Проверка при загрузке
+  checkFormValidity();
+};
+
+
+/**
+Password Reset Query Form Validator
+*/
+function validateResetPassordForm() {
+  const form = document.querySelector('.reset-password-form');
+  if (!form) return;
+
+  const emailInput = form.querySelector('.user-email');
+  const submitBtn = form.querySelector('button[type="submit"]')
+
+  // Отключить кнопку submit 
+  submitBtn.disabled = true;
+  // Проверить валидность полей
+  function checkFormValidity() {
+    let allValid = true;   
+    // Валидация email
+    const emailValid = validateEmail(emailInput.value);
+    if (!emailValid) allValid = false;
+    // Активируем или деактивируем кнопку
+    submitBtn.disabled = !allValid;
+  };
+  // Добавляем обработчики событий на поля ввода
+  emailInput.addEventListener('input', checkFormValidity);
+  // Проверка при загрузке
+  checkFormValidity();
+};
+
+
+/**
+Registration Page Validator
+*/
+function validateRegisterForm() {
+  const form = document.querySelector('.user-register');
+  if (!form) return;
+
+  const nameInputs = form.querySelectorAll('.user-names')
+  const emailInput = form.querySelector('.user-email');
+  const passwdInputs = form.querySelectorAll('.user-password')
+  const submitBtn = form.querySelector('button[type="submit"]')
+  // Отключить кнопку submit 
+  submitBtn.disabled = true;
+  // Проверить валидность полей
+  function checkFormValidity() {
+    let allValid = true;   
+    // Валидация имен
+    nameInputs.forEach(input => {
+      const valid = validateName(input.value);
+      if (!valid) allValid = false;
+    });
+    // Валидация email
+    const emailValid = validateEmail(emailInput.value);
+    if (!emailValid) allValid = false;
+    // Валидация password
+    passwdInputs.forEach(input => {
+      const valid = validatePassword(input.value);
+      if (!valid) allValid = false;
+    });
+    // Активируем или деактивируем кнопку
+    submitBtn.disabled = !allValid;
+  };
+  // Добавляем обработчики событий на поля ввода
+  emailInput.addEventListener('input', checkFormValidity);
+  nameInputs.forEach(input => input.addEventListener('input', checkFormValidity));
+  passwdInputs.forEach(input => input.addEventListener('input', checkFormValidity));
+  // Проверка при загрузке
+  checkFormValidity();
+};
+
+
+/**
+Login Page Validator
+*/
+function validateLoginForm() {
+  const form = document.querySelector('.login-form');
+  if (!form) return;
+
+  const emailInput = form.querySelector('.user-email');
+  const passwdInput = form.querySelector('.user-password')
+  const submitBtn = form.querySelector('button[type="submit"]')
+  // Отключить кнопку submit 
+  submitBtn.disabled = true;
+  // Проверить валидность полей
+  function checkFormValidity() {
+    let allValid = true;   
+    // Валидация email
+    const emailValid = validateEmail(emailInput.value);
+    if (!emailValid) allValid = false;
+    // Валидация password
+    const passwdValid = validatePassword(passwdInput.value);
+    if (!passwdValid) allValid = false;
+    // Активируем или деактивируем кнопку
+    submitBtn.disabled = !allValid;
+  };
+  // Добавляем обработчики событий на поля ввода
+  emailInput.addEventListener('input', checkFormValidity);
+  passwdInput.addEventListener('input', checkFormValidity);
+  // Проверка при загрузке
+  checkFormValidity();
+};
+
+
+/**
+Wardrobe Calculator Page Validator
+*/
 function validateWardrobeForm() {
   const form = document.querySelector('.wardrobe');
   if (!form) return;
@@ -19,11 +157,10 @@ function validateWardrobeForm() {
 
   function checkFormValidity() {
     let allValid = true;
-
     inputs.forEach(input => {
       const valid = isValid(input.value);
-      input.classList.toggle('is-invalid', !valid);
-      input.classList.toggle('is-valid', valid);
+      // input.classList.toggle('is-invalid', !valid);
+      // input.classList.toggle('is-valid', valid);
 
       if (!valid) allValid = false;
     });
@@ -32,20 +169,17 @@ function validateWardrobeForm() {
       submitBtn.disabled = !allValid;
     }
   }
-
   // Инициализируем валидацию при вводе
   inputs.forEach(input => {
     input.addEventListener('input', checkFormValidity);
   });
-
   // Проверка при загрузке
   checkFormValidity();
 };
 
 
-// Функции валидаторы
 /**
-Validator Save Order Page
+Save Order Page Validator
 */
 function validateSaveOrderForm() {
   const form = document.querySelector('.saveorder') 
@@ -66,20 +200,20 @@ function validateSaveOrderForm() {
     // Валидация имен
     nameInputs.forEach(input => {
       const valid = validateName(input.value);
-      input.classList.toggle('is-invalid', !valid);
-      input.classList.toggle('is-valid', valid);
+      // input.classList.toggle('is-invalid', !valid);
+      // input.classList.toggle('is-valid', valid);
       if (!valid) allValid = false;
     })
     // Валидация телефона
     const phoneValid = validatePhoneNumber(phoneInput.value);
-    phoneInput.classList.toggle('is-invalid', !phoneValid);
-    phoneInput.classList.toggle('is-valid', phoneValid);
+    // phoneInput.classList.toggle('is-invalid', !phoneValid);
+    // phoneInput.classList.toggle('is-valid', phoneValid);
     if (!phoneValid) allValid = false;
 
     // Валидация email
     const emailValid = validateEmail(emailInput.value);
-    emailInput.classList.toggle('is-invalid', !emailValid);
-    emailInput.classList.toggle('is-valid', emailValid);
+    // emailInput.classList.toggle('is-invalid', !emailValid);
+    // emailInput.classList.toggle('is-valid', emailValid);
     if (!emailValid) allValid = false;
 
     // Активируем или деактивируем кнопку
@@ -128,4 +262,13 @@ Email validation func.
 function validateEmail(email) {
   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailPattern.test(email);  
+}
+
+/**
+Password validation func.
+@param {str} password - password 
+@returns {boolean}
+*/
+function validatePassword(password) {
+  return password.length >= 6;  
 }
