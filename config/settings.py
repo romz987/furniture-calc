fa29e2207068ccd47b3ec1ab95fc8e062438bee9
romz987pc <eesistance@gmail.com>
2025-07-13 10:51:10 +0300

    fix: email host access to .env;

--- config/settings.py
+++ config/settings.py
@@ -162,8 +162,8 @@ LOGIN_URL = '/user/login/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.com'
 EMAIL_PORT = 465
-EMAIL_HOST_USER = 'top-academy-roman@yandex.ru'
-EMAIL_HOST_PASSWORD = 'gogulkfopyjglzpv'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 
